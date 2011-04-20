ee4545e3a444dc59248ca79bbb23ffada9c100cb
Tom Duckering <tduckeri@thoughtworks.com>
2011-04-20 14:05:17 +0200

    updating the build script to run under jenkins and adding some changes to repo service

--- src/org/duckering/restrepo/rest/RestRepoService.java
+++ src/org/duckering/restrepo/rest/RestRepoService.java
@@ -1,17 +1,26 @@
 package org.duckering.restrepo.rest;
 
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+import org.apache.cxf.jaxrs.ext.multipart.ContentDisposition;
+import org.apache.cxf.jaxrs.ext.multipart.MultipartBody;
 import org.duckering.restrepo.Artifact;
 import org.duckering.restrepo.ArtifactRepository;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @Path("/artifact")
 public class RestRepoService {
 
-    private @Context UriInfo uriInfo;
+    private
+    @Context
+    UriInfo uriInfo;
     private ArtifactRepository artifactRepository;
 
     public RestRepoService(ArtifactRepository artifactRepository) {
@@ -24,12 +33,22 @@ public class RestRepoService {
     public Response doGet(@PathParam("artifactId") int artifactId) {
 
         Artifact artifact = artifactRepository.get(artifactId);
-        
+
         return Response.ok().entity(artifact).build();
     }
 
     @PUT
-    public Response doPut() {
+    @Produces("application/vnd.restrepo+xml")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response doPut(MultipartBody multipartBody) {
+        Attachment a = multipartBody.getAttachment("file");
+        ContentDisposition cd = a.getContentDisposition();
+        try {
+            InputStream is = a.getDataHandler().getInputStream();
+        } catch (IOException e) {
+            return Response.serverError().build();
+        }
+
         return Response.ok().build();
     }
 }
